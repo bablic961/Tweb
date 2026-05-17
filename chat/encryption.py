098d1ab9098c703dd1953167f4f1f40fffd4f865
bablic961 <engcvantum@gmail.com>
2026-05-17 19:11:23 +0300

    Update encryption.py

--- chat/encryption.py
+++ chat/encryption.py
@@ -1,23 +1,22 @@
-from cryptography.fernet import Fernet
-from django.conf import settings
-import hashlib
 import base64
+import hashlib
+from cryptography.fernet import Fernet
+
+# ЕДИНЫЙ КЛЮЧ ШИФРОВАНИЯ (задаём жёстко)
+MASTER_KEY = 'TeleWebSecretKey2026!'
 
 def get_chat_key(chat_id, user1_id, user2_id):
-    """Генерирует ключ шифрования"""
     ids = sorted([str(user1_id), str(user2_id)])
-    raw = f"{chat_id}:{ids[0]}:{ids[1]}:{settings.SECRET_KEY}"
+    raw = f"{chat_id}:{ids[0]}:{ids[1]}:{MASTER_KEY}"
     key = hashlib.sha256(raw.encode()).digest()
     return base64.urlsafe_b64encode(key)
 
 def encrypt_message(chat_id, sender_id, receiver_id, text):
-    """Шифрует сообщение"""
     key = get_chat_key(chat_id, sender_id, receiver_id)
     f = Fernet(key)
     return f.encrypt(text.encode())
 
 def decrypt_message(chat_id, user1_id, user2_id, encrypted_text):
-    """Расшифровывает сообщение"""
     try:
         key = get_chat_key(chat_id, user1_id, user2_id)
         f = Fernet(key)
