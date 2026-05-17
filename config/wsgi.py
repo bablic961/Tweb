2de637c332a24084f93bb696127ea2a8760a815b
bablic961 <engcvantum@gmail.com>
2026-05-17 21:37:41 +0300

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,4 +2,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Создаём все таблицы при запуске
+import django
+django.setup()
+from django.core.management import call_command
+call_command('migrate', '--run-syncdb', '--noinput')
+
 application = get_wsgi_application()
