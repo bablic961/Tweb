ed9b74ab9e8d32dadd049a5cb4bb95ab7aada298
bablic961 <engcvantum@gmail.com>
2026-05-17 21:13:03 +0300

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,11 +2,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-# Выполняем миграции
-import django
-django.setup()
-from django.core.management import call_command
-call_command('migrate', '--noinput')
-
 application = get_wsgi_application()
